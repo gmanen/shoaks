ea205576daddec41c38819f7035c6e6c3a25b7dc
Guillaume Manen <guillaume.manen@gmail.com>
2025-11-14 12:45:05 +0100

    chore: upgrade mocha and add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {},
+}
